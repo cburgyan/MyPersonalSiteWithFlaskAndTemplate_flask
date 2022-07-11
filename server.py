589b5aef01e29d116a246a4201081d96835bf196
cburgyan <cburgyan@gmail.com>
2022-07-11 13:11:14 -0500

    create and add favicon.ico

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ the_app = Flask(__name__)
 
 @the_app.route('/')
 def intro_page():
-    return render_template('charles.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
