1ea4bb2366c6713ebd51044a5c2a8f8dfe4c693b
cburgyan <cburgyan@gmail.com>
2022-07-12 12:54:38 -0500

    import website template

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ the_app = Flask(__name__)
 
 @the_app.route('/')
 def intro_page():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 
 if __name__ == "__main__":
